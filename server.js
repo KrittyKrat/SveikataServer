8f072e8b07544bff8c879a86647e6f96db2d431a
KrittyKrat <justas.dap@gmail.com>
2022-11-06 21:12:29 +0200

    port

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.get('/specialists', async (req, res) => {
     }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
