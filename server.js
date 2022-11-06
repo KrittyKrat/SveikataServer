99761f84748c0209029093301e7281f5a7649342
KrittyKrat <justas.dap@gmail.com>
2022-11-06 21:24:44 +0200

    aaaaaaaa

--- server.js
+++ server.js
@@ -16,17 +16,17 @@ mongoose.connect(
 
 app.use(express.json());
 
-const institutionRouter = require('./routes/institutions');
-app.use('/institutions', institutionRouter);
+//const institutionRouter = require('./routes/institutions');
+//app.use('/institutions', institutionRouter);
 
-const departmentRouter = require('./routes/departments');
-app.use('/institutions/:institutionID/departments', departmentRouter);
+//const departmentRouter = require('./routes/departments');
+//app.use('/institutions/:institutionID/departments', departmentRouter);
 
-const specialistRouter = require('./routes/specialists');
-app.use('/institutions/:institutionID/departments/:departmentID/specialists', specialistRouter);
+//const specialistRouter = require('./routes/specialists');
+//app.use('/institutions/:institutionID/departments/:departmentID/specialists', specialistRouter);
 
-const userRouter = require('./routes/users');
-app.use('/users', userRouter);
+//const userRouter = require('./routes/users');
+//app.use('/users', userRouter);
 
 app.get('/departments', async (req, res) => {
     try{
